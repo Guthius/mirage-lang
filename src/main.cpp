cb84f552efa7c6896c6466fb2d3281c9452c554b
Guthius <dhummel@gmail.com>
2026-06-27 23:03:16 +0300

    parser: add start.mir and cleanup main

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,7 @@
 #include "Compiler/lexer.hpp"
 #include "Compiler/module_resolver.hpp"
 
-#include <fstream>
-#include <iostream>
-#include <sstream>
 #include <string>
-
-namespace {
-    auto ReadFile(const std::string &path) -> std::string {
-        std::ifstream ifs(path);
-        if (!ifs) {
-            std::cerr << "cannot open file '" << path << "'\n";
-            std::exit(1);
-        }
-        std::ostringstream ss;
-        ss << ifs.rdbuf();
-        return ss.str();
-    }
-}
 
 auto main(const int argc, char *argv[]) -> int {
     if (argc != 2) {
